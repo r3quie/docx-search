3f331af6b872496e35b48a717d4d290e6581cc93
r3quie <r3quie@gmail.com>
2024-07-01 15:39:10 +0200

    ic rc

--- search.py
+++ search.py
@@ -1,41 +1,65 @@
 from docx import Document
 import os
 
-def search(string: str, substring: str):
+def is_In(string: str, substring: str):
     if substring in string:
         return True
     else:
         return False
 
-def searcha(docx_file: str, search_strings: tuple):
+def search(docx_file: str, search_strings: tuple, icrc: bool = None):
     doc = Document(docx_file)
     foundnum = 0
+    human_index = 0
     for a in search_strings:
-        wasFound = False
         for par in doc.paragraphs:
-            if search(par.text, a):
+            if is_In(par.text, "RČ"):
+                human_index += 1
+            if is_In(par.text, "IČ"):
+                human_index -= 1
+            if is_In(par.text, a):
                 foundnum += 1
                 break
 
-    if foundnum >= len(search_strings):
+    if foundnum < len(search_strings):
+        return False
+    if icrc is None:
+        return True
+
+    if human_index > 0 and icrc:
+        return True
+    elif human_index < 0 and not icrc:
         return True
     else:
         return False
 
-def main(folder: str, search_strings: tuple):
+def main(folder: str, search_strings: tuple, icrc: bool = None):
+    gudfiles = []
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith('.docx'):
                 f = open(str(os.path.join(root, file)), 'rb')
                 try:    
-                    if searcha(f, search_strings):
+                    if search(f, search_strings, icrc):
                         print(os.path.join(root, file))
+                        gudfiles += [os.path.join(root, file)]
                     f.close()
                 except Exception as e:
                     f.close()
 
+    return gudfiles
+
+
+
 if __name__ == '__main__':
-    main(input("filepath"), (input("str1"), input('str2'), input('str3')))
+    gotten = input('ic nebo rc')
+    if gotten.startswith('i') or gotten.startswith('I'):
+        gotten = False
+    elif gotten.startswith('r') or gotten.startswith('R'):
+        gotten = True
+    else:
+        gotten = None
+    main(input("filepath"), (input("str1"), input('str2'), input('str3')), icrc = gotten)
 
 
             
